9dc5a95d7593e3c6a9e819d3211a3d2eadb732a5
anyakul <anyakulikova111@gmail.com>
2020-01-05 21:09:04 +0300

    Наследую компонент формы редактирования от AbstractSmartComponent

--- src/components/event-editor.js
+++ src/components/event-editor.js
@@ -1,25 +1,19 @@
-import AbstractComponent from './abstract-component';
+import AbstractSmartComponent from './abstract-smart-component';
 import {createEventEditorTemplate} from './templates/event-editor';
 import {showData} from './event-card';
 
-class EventEditorComponent extends AbstractComponent {
+class EventEditorComponent extends AbstractSmartComponent {
   constructor(events) {
     super();
     this._card = showData(events);
-    this._submitHandler = [];
   }
 
   getTemplate() {
     return createEventEditorTemplate(this._card);
   }
 
-  rerender() {
-    super.rerender();
-  }
-
   setSubmitHandler(handler) {
     this.getElement().querySelector(`form`).addEventListener(`submit`, handler);
-    this._submitHandler = handler;
   }
 
   setFavoriteButtonClickHandler(handler) {
@@ -31,7 +25,10 @@ class EventEditorComponent extends AbstractComponent {
     this.getElement().querySelector(`.event__rollup-btn`)
       .addEventListener(`click`, handler);
     this._rollUpButtonClickHandler = handler;
-    this._submitHandler = handler;
+  }
+
+  recoveryListeners() {
+    
   }
 }
 
