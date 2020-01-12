3add0178b9b6102fb8597cddabf3e4f4d2a5c9cc
anyakul <anyakulikova111@gmail.com>
2020-01-13 02:07:58 +0300

    Подключаю модель в точку входа

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import 'flatpickr/dist/flatpickr.min.css';
 import TripController from './controllers/trip.js';
 import {events} from './mock/trip-event';
+import EventsModel from './models/points.js';
 
 const body = document.querySelector(`body`);
+
+const eventsModel = new EventsModel();
+eventsModel.setEvents(events);
 
 const tripController = new TripController(body);
 tripController.render(events);
